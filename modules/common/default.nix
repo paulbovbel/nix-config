c25ef7700e421444a6e25027c20771403f894460
Paul Bovbel <paul@bovbel.com>
2026-05-22 16:01:15 -0400

    Update age secret mappings and scope the pbovbel SSH key to the user module.

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -39,11 +39,26 @@
     X11Forwarding = false;
   };
 
-  # TODO(pbovbel): Re-enroll during deploy by minting a short-lived auth key
-  # externally, then running tailscale up over SSH to avoid persistent auth keys
-  # on hosts.
-  services.tailscale.enable = true;
-  services.tailscale.openFirewall = true;
+  users.users.deploy = {
+    isNormalUser = true;
+    description = "Deployment user";
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1WGjxe/6kJ2uHiI1R85VifWC2GeaEj98sAZIMLtFqgqY8zASg7in+We4oE/H1xBPf9AXHwM03rNTNQyVQ/w+YRacPAiRI8w6/tnx+ry/atxwZjFuGgYvzJockc1ar3zGSa3TWWUqe85TfwB6YjbQtSqqvGQ+BWI44+nsbKgGtFzyVyBBhdYmuBcVkNi9rCATRtto4rmBEs9RfHvWb+dLXMdUZbo4DsYZanMiucbWkrq4soHVZKJWGMqBmVRwVsO+pm9FyE3p1EaRh5afILCKi0X3X3jdJUrWIqqn7SiqaQCrx4uotpQef0S45eJhl2AqwpB66OnngMfhB4xaO+wgBEpjOheLLcfnFCH9WXEmD6r49om91K22+8j20Y93zNeDoYC6OYxe0flAzdsTbyfyx2lo2/TdNzYc5ruqgNbnhDnbeZJ2JLx3CbpixxGZJU9BhG2Pye+dpgnLTT48jEX5L/kWQMNkD50mpIEbFK8zLASH5g1q5bvw0NuTrpN8u2FqCmPEvpybFOTw1lV13I0l2fCdHSw3RNPA3QSP/GeGbOkx7yGWH2wJxJTGr1up2FBp7S6uCqU7MlVlrRbSzyKEmH5cTTFho+CnAhr1lQtlajCRTwm5UuoQYLFYkT/J+1lcXqU40H7jKYqRdwgVZ5CL6smJ/9IuZiJY2CA3rmcrPFQ== paul@bovbel.com"
+    ];
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "deploy" ];
+      commands = [
+        {
+          command = "ALL";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
   nixpkgs.config.allowUnfree = true;
 
@@ -51,6 +66,7 @@
     pkgs.bat
     pkgs.bind
     pkgs.curl
+    pkgs.ethtool
     pkgs.eza
     pkgs.fd
     pkgs.git
@@ -62,6 +78,7 @@
     pkgs.kitty.terminfo
     pkgs.mtr
     pkgs.nettools
+    pkgs.ncdu
     pkgs.nethogs
     pkgs.ripgrep
     pkgs.tcpdump
