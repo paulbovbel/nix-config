ecd2da45c01d816f189e67d6534aea3ed936dcaa
Paul Bovbel <paul@bovbel.com>
2026-06-05 20:36:25 -0400

    Newer nvidia on white-tower

--- hosts/white-tower/configuration.nix
+++ hosts/white-tower/configuration.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -18,6 +22,14 @@
   };
 
   nvidia.sleep.enable = true;
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "610.43.02";
+    sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+    openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+    settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+    persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+  };
 
   system.stateVersion = "26.05";
 }
