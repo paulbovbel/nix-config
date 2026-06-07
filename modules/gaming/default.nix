0d27400710e95fb8737acefe3d58ce6931546d21
Paul Bovbel <paul@bovbel.com>
2026-06-06 22:20:12 -0400

    Tune sunshine for nvidia streaming

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -53,12 +53,67 @@
     autoStart = true;
     openFirewall = true;
     capSysAdmin = true;
+    package = pkgs.sunshine.override {cudaSupport = true;};
+
+    settings = {
+      port = 47989;
+
+      capture = "kms";
+      encoder = "nvenc";
+      hevc_mode = 3;
+      av1_mode = 1;
+
+      nvenc_preset = 5;
+      nvenc_twopass = "quarter_res";
+      nvenc_spatial_aq = "enabled";
+      nvenc_vbv_increase = 200;
+
+      max_bitrate = 150000;
+      minimum_fps_target = 60;
+      lan_encryption_mode = 0;
+    };
+
+    applications = {
+      env.PATH = "$(PATH):$(HOME)/.local/bin";
+      apps = [
+        {
+          name = "Desktop";
+          image-path = "desktop.png";
+        }
+        {
+          name = "Steam Big Picture";
+          detached = [
+            "setsid /run/current-system/sw/bin/steam steam://open/bigpicture"
+          ];
+          prep-cmd = [
+            {
+              do = "";
+              undo = "setsid /run/current-system/sw/bin/steam steam://close/bigpicture";
+            }
+          ];
+          image-path = "steam.png";
+        }
+        {
+          name = "Heroic";
+          detached = [
+            "${pkgs.heroic}/bin/heroic"
+          ];
+        }
+        {
+          name = "Lutris";
+          detached = [
+            "${pkgs.lutris}/bin/lutris"
+          ];
+        }
+      ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
     mangohud
     goverlay
     gamescope
+    heroic
     protonup-qt
     lutris
   ];
