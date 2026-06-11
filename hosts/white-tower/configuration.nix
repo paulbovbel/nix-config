c7f316d623351e0f90f0e528c450d1ac7c033ae4
Paul Bovbel <paul@bovbel.com>
2026-06-10 21:57:15 -0400

    Add audio rules for white-tower

--- hosts/white-tower/configuration.nix
+++ hosts/white-tower/configuration.nix
@@ -23,6 +23,32 @@
 
   nvidia.sleep.enable = true;
 
+  services.udev.extraRules = ''
+    # Prevent the Audeze Maxwell dongle from autosuspending mid-session.
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="3329", ATTR{idProduct}=="4b19", TEST=="power/control", ATTR{power/control}="on"
+  '';
+
+  services.pipewire.wireplumber.extraConfig."51-hide-audio-devices" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          {
+            "device.name" = "alsa_card.pci-0000_07_00.1";
+          }
+        ];
+        actions.update-props."device.disabled" = true;
+      }
+      {
+        matches = [
+          {
+            "device.name" = "alsa_card.pci-0000_09_00.4";
+          }
+        ];
+        actions.update-props."device.profile" = "output:iec958-stereo";
+      }
+    ];
+  };
+
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     version = "610.43.02";
     sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
