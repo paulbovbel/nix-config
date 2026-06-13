b6f29f33a641c1fa337ba4a29023aa3b8dcc885b
Paul Bovbel <paul@bovbel.com>
2026-06-13 16:50:35 -0400

    Simplify nvidia module, deal with suspend issues in white-tower host config

--- modules/nvidia/options.nix
+++ modules/nvidia/options.nix
@@ -1,11 +1,5 @@
 {lib, ...}: {
   options.nvidia = {
-    sleep.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable NVIDIA sleep, suspend, hibernate, and wake-inhibit integration.";
-    };
-
     prime = {
       enable = lib.mkOption {
         type = lib.types.bool;
