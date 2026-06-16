9bcc594c24e44505b382d9679fb4ab80a4c6b41b
Paul Bovbel <paul@bovbel.com>
2026-06-15 23:14:52 -0400

    Remove snapdir; Add netboot image to EFI; re-enable impermanence

--- modules/impermanence-root/system.nix
+++ modules/impermanence-root/system.nix
@@ -24,22 +24,22 @@ in {
 
     systemd.timers.zfs-snapshot-frequent.wantedBy = lib.mkForce [];
 
-    # boot.initrd.systemd.services.zfs-rollback-root = {
-    #   description = "Rollback zroot/root to @blank snapshot";
-    #   wantedBy = ["initrd.target"];
-    #   after = ["zfs-import-zroot.service"];
-    #   # Must run before / is mounted so rollback applies to the live root.
-    #   before = ["sysroot.mount"];
-    #   unitConfig.DefaultDependencies = false;
-    #   serviceConfig.Type = "oneshot";
-    #   script = ''
-    #     if zfs list -t snapshot -H -o name ${cfg.rootDataset}@${cfg.blankSnapshot} >/dev/null 2>&1; then
-    #       echo "zfs-rollback-root: rolling back ${cfg.rootDataset}@${cfg.blankSnapshot}"
-    #       zfs rollback -r ${cfg.rootDataset}@${cfg.blankSnapshot}
-    #     else
-    #       echo "zfs-rollback-root: WARNING snapshot ${cfg.rootDataset}@${cfg.blankSnapshot} not found; skipping rollback"
-    #     fi
-    #   '';
-    # };
+    boot.initrd.systemd.services.zfs-rollback-root = {
+      description = "Rollback zroot/root to @blank snapshot";
+      wantedBy = ["initrd.target"];
+      after = ["zfs-import-zroot.service"];
+      # Must run before / is mounted so rollback applies to the live root.
+      before = ["sysroot.mount"];
+      unitConfig.DefaultDependencies = false;
+      serviceConfig.Type = "oneshot";
+      script = ''
+        if zfs list -t snapshot -H -o name ${cfg.rootDataset}@${cfg.blankSnapshot} >/dev/null 2>&1; then
+          echo "zfs-rollback-root: rolling back ${cfg.rootDataset}@${cfg.blankSnapshot}"
+          zfs rollback -r ${cfg.rootDataset}@${cfg.blankSnapshot}
+        else
+          echo "zfs-rollback-root: WARNING snapshot ${cfg.rootDataset}@${cfg.blankSnapshot} not found; skipping rollback"
+        fi
+      '';
+    };
   };
 }
