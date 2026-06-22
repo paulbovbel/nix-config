140eecb7d7e50a82878dde9c7d5d0ff312176c41
Paul Bovbel <paul@bovbel.com>
2026-06-22 18:08:05 -0400

    Add 404 page

--- modules/caddy/caddyfile.nix
+++ modules/caddy/caddyfile.nix
@@ -165,7 +165,79 @@
         }
       }
 
+    (not-found) {
+      route {
+        header Content-Type text/html
+        respond <<HTML
+          <!doctype html>
+          <html lang="en">
+
+          <head>
+            <meta charset="utf-8">
+            <title>Page Not Found</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style>
+              * {
+                line-height: 1.2;
+                margin: 0;
+              }
+
+              html {
+                color: #888;
+                display: table;
+                font-family: sans-serif;
+                height: 100%;
+                text-align: center;
+                width: 100%;
+              }
+
+              body {
+                display: table-cell;
+                vertical-align: middle;
+                margin: 2em auto;
+              }
+
+              h1 {
+                color: #555;
+                font-size: 2em;
+                font-weight: 400;
+              }
+
+              p {
+                margin: 0 auto;
+                width: 280px;
+              }
+
+              @media only screen and (max-width: 280px) {
+
+                body,
+                p {
+                  width: 95%;
+                }
+
+                h1 {
+                  font-size: 1.5em;
+                  margin: 0 0 0.3em;
+                }
+
+              }
+            </style>
+          </head>
+
+          <body>
+            <h1>Page Not Found</h1>
+            <p>Sorry, but the page you were trying to view does not exist.</p>
+          </body>
+
+          </html>
+          <!-- IE needs 512+ bytes: https://docs.microsoft.com/archive/blogs/ieinternals/friendly-http-error-pages -->
+          HTML 404
+      }
+    }
+
     *.${cfg.publicDomain} {
+      import not-found
+
       import public-tls
     }
 
@@ -186,7 +258,9 @@
           authenticate with defaultportal
         }
 
-    ${lib.concatMapStrings renderEndpoint endpoints}}
+    ${lib.concatMapStrings renderEndpoint endpoints}
+        import not-found
+      }
 
       ${primaryDomain} {
         import primary-site
