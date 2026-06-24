2b44b9f14c7423952d3264aa15d2e7df72208084
Paul Bovbel <paul@bovbel.com>
2026-06-24 01:12:49 -0400

    Break up profiles/common

---
+++ profiles/common/services.nix
@@ -0,0 +1,61 @@
+{pkgs, ...}: let
+  inhibitSleepWhileSshScript = ./inhibit-sleep-while-ssh.sh;
+in {
+  systemd.services = {
+    inhibit-sleep-while-ssh = {
+      description = "Prevent system sleep while interactive SSH sessions are active";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target"];
+      path = [pkgs.systemd pkgs.bash pkgs.procps pkgs.coreutils pkgs.gnugrep];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 5;
+        ExecStart = "${pkgs.bash}/bin/bash ${inhibitSleepWhileSshScript}";
+      };
+    };
+  };
+
+  networking = {
+    resolvconf.enable = false;
+  };
+
+  services = {
+    fwupd.enable = true;
+
+    resolved = {
+      enable = true;
+      settings.Resolve.ResolveUnicastSingleLabel = true;
+    };
+
+    openssh = {
+      enable = true;
+      settings = {
+        Port = 22;
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        PermitEmptyPasswords = false;
+        StrictModes = true;
+        IgnoreRhosts = true;
+        UsePAM = true;
+        KbdInteractiveAuthentication = false;
+        X11Forwarding = false;
+      };
+      hostKeys = [
+        {
+          path = "/persist/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+        {
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+    };
+  };
+
+  impermanenceRoot.persistDirectories = [
+    "/var/lib/fwupd"
+  ];
+}
