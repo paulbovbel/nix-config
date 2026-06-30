da3e00354daa632beef6f4396c29a4444348eca5
Paul Bovbel <paul@bovbel.com>
2026-06-30 11:33:13 -0400

    Fix awscli2

--- profiles/common/cli-packages.nix
+++ profiles/common/cli-packages.nix
@@ -1,7 +1,7 @@
 {pkgs}: let
   packagePairs = [
     ["age" pkgs.age]
-    ["awscli" pkgs.awscli]
+    ["awscli" pkgs.awscli2]
     ["bat" pkgs.bat]
     ["bind9-dnsutils" pkgs.bind]
     ["ca-certificates" pkgs.cacert]
