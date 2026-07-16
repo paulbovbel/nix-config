23f0d18a28db1d0853fdb9d50514d0669a378f5e
Paul Bovbel <paul@bovbel.com>
2026-07-16 19:27:44 -0400

    Patch prismlauncher

--- flake.nix
+++ flake.nix
@@ -92,6 +92,16 @@
           };
         });
 
+        prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (oldAttrs: {
+          postPatch =
+            (oldAttrs.postPatch or "")
+            + ''
+              substituteInPlace launcher/minecraft/auth/MinecraftAccount.h \
+                --replace-fail 'bool ownsMinecraft() const { return data.type != AccountType::Offline && data.minecraftEntitlement.ownsMinecraft; }' \
+                               'bool ownsMinecraft() const { return true; }'
+            '';
+        });
+
         netbootxyz-efi = prev.netbootxyz-efi.overrideAttrs (_: {
           version = "3.0.2";
           src = final.fetchurl {
