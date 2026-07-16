b904d6098c0b4f344c335d8df459bd232160fc1b
Paul Bovbel <paul@bovbel.com>
2026-07-16 14:29:58 -0400

    Add rainbow-wave computer

---
+++ hosts/rainbow-wave/configuration.nix
@@ -0,0 +1,33 @@
+{pkgs, ...}: {
+  imports = [
+    ../site.nix
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    # TODO try CachyOS kernel for gaming performance?
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  networking = {
+    hostName = "rainbow-wave";
+    hostId = "4619f943";
+  };
+
+  rootZfs = {
+    enable = true;
+    impermanent = true;
+    swapSize = "8G";
+  };
+
+  nvidia.enable = true;
+
+  system.stateVersion = "26.05";
+
+  netboot = {
+    enable = true;
+    installLegacyImage = true;
+  };
+}
