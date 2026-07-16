2c2d08972e3d0780c9ae46b1e65eed99771f2891
Paul Bovbel <paul@bovbel.com>
2026-07-16 19:29:31 -0400

    Add prismlauncher to favorites

--- users/pbovbel/gaming.nix
+++ users/pbovbel/gaming.nix
@@ -8,6 +8,7 @@ in {
 
   dconf.settings."org/gnome/shell".favorite-apps = lib.mkAfter [
     "steam.desktop"
+    "org.prismlauncher.PrismLauncher.desktop"
     "com.discordapp.Discord.desktop"
   ];
 
