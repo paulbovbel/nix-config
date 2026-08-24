4b099077af3e2bc069fbd3574f636cb830b4b58a
Paul Bovbel <paul@bovbel.com>
2026-08-24 00:50:05 -0400

    Update nvidia feature driver

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -18,11 +18,11 @@ in {
         nvidiaSettings = true;
         # package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.latest;
         package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "610.43.02";
-          sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
-          openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
-          settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
-          persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+          version = "610.57.04";
+          sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+          openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
+          settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
+          persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
         };
         powerManagement.enable = true;
         powerManagement.finegrained = lib.mkDefault false;
