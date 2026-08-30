feaa96316fdfd84a9534965dd0746c942890ba8a
Paul Bovbel <paul@bovbel.com>
2026-08-29 21:36:49 -0400

    Switch nvidia driver

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -16,14 +16,14 @@ in {
         modesetting.enable = true;
         inherit (cfg) open;
         nvidiaSettings = true;
-        package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.latest;
-        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        #   version = "610.57.04";
-        #   sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
-        #   openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
-        #   settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
-        #   persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
-        # };
+        # package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.latest;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "610.57.04";
+          sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+          openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
+          settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
+          persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
+        };
         powerManagement.enable = true;
         powerManagement.finegrained = lib.mkDefault false;
       };
