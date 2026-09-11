51c60e0a1f4cb46cd8454c02f31d2deda97fc69f
Paul Bovbel <paul@bovbel.com>
2026-09-11 11:00:27 -0400

    Ubuntu dropped awscli package

--- profiles/common/cli-packages.nix
+++ profiles/common/cli-packages.nix
@@ -1,7 +1,6 @@
 {pkgs}: let
   packagePairs = [
     ["age" pkgs.age]
-    ["awscli" pkgs.awscli2]
     ["bat" pkgs.bat]
     ["bind9-dnsutils" pkgs.bind]
     ["ca-certificates" pkgs.cacert]
--- profiles/common/core.nix
+++ profiles/common/core.nix
@@ -56,6 +56,7 @@ in {
     ]
     ++ cliPackages.nixPackages
     ++ [
+      pkgs.awscli2
       pkgs.dust
       pkgs.eza
       pkgs.jless
